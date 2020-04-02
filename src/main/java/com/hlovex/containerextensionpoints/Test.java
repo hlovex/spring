a3275b68a3443aac5b2df7dd12a920da1333431f
hlovex <hlovex1314520@163.com>
2020-04-02 23:26:23 +0800

    PropertySourcesPlaceholderConfigurer

--- src/main/java/com/hlovex/containerextensionpoints/Test.java
+++ src/main/java/com/hlovex/containerextensionpoints/Test.java
@@ -1,14 +1,13 @@
 package com.hlovex.containerextensionpoints;
 
-import com.hlovex.beandefinitioninheritance.DerivedTestBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Test {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("containerextensionpoints.xml");
-        Messenger messenger = context.getBean(Messenger.class);
-        System.out.println(messenger);
+//        HikariDataSource dataSource = context.getBean(HikariDataSource.class);
+//        System.out.println(dataSource.getJdbcUrl());;
     }
 
 }
